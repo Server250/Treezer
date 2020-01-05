25dfb0cc7e6ac3dd4c3ca547abffbf030897f0a0
= <=>
2020-01-05 20:33:48 +0000

    Expanded Element datatype, altered parseTags to find multiple tags. It sort of works, needs nesting.

--- treezer.py
+++ treezer.py
@@ -2,23 +2,34 @@ import re
 
 class Element:
 
-	def __init__(self,name):
-		self.name=name;
-		print("Element created: " + self.name)
+	def __init__(self,name,children):
+		self.name=name
+		self.children=[]
+		self.properties=[]
+		self.content=""
+		#print("Element created: " + self.name)
 		
 def parseTags(content):
 
-	# Matches: open tag, a word, spaces, as many attributes (making sure quotes match)	
-	openTagRe = re.compile(r'<(\w+)(\s(\w+(=(?P<quote>\"|\').*(?P=quote)?))?)*>')
-	openTag = re.search(openTagRe,content)	
+	if ((not content) or (content == "")):
+		return
 
+	# TODO: Self-closing tags
+
+	# Matches: open tag, a word, spaces, as many attributes (making sure quotes match)	
+	openTagRe = re.compile(r'<(\w+)(\s(\w+(=(?P<quote>\"|\').*(?P=quote)?))?)*>', re.IGNORECASE)
+	openTag = re.search(openTagRe,content)	
+	
 	# find closing tag for new tag
-	closeTagRe = re.compile(r'<\/('+openTag.group(1)+')>')
+	closeTagRe = re.compile(r'<\/('+openTag.group(1)+')>', re.IGNORECASE)
 	closeTag = re.search(closeTagRe,content)	
-	print("Open tag: " + openTag.group(0))
-	print("Close tag: " + closeTag.group(0))	
-	# run parsetags on all internal content	
+	#print("Open tag: " + openTag.group(0))
+	#print("Close tag: " + closeTag.group(0))		
+
+	internalContent = content[openTag.span()[1]:closeTag.span()[0]]	
+	return Element(openTag.group(1),parseTags(internalContent))
 
 if __name__=="__main__":
 	print("Treezer running.")
-	parseTags("<body></body>")
+	print("Root tag: " + parseTags("<html><body><img><a></a></img><img></img></body></html>").name)
+	print("Treezer completed.")
